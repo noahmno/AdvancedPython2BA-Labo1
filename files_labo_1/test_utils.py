4ee44a1c235cdf8a4ecf1dcd6b062b030e819a41
noah <22294@ecam.be>
2025-03-17 13:45:58 +0100

    init

---
+++ files_labo_1/test_utils.py
@@ -0,0 +1,24 @@
+# test_utils.py
+# Author: Sébastien Combéfis
+# Version: February 8, 2018
+
+import unittest
+import utils
+
+class TestUtils(unittest.TestCase):
+    def test_fact(self):
+       
+        pass
+    
+    def test_roots(self):
+       
+        pass
+    
+    def test_integrate(self):
+       
+        pass
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
+    runner = unittest.TextTestRunner()
+    exit(not runner.run(suite).wasSuccessful())
